60dcc901b141e163deca02f43d4035038e766e4c
Divakar Saini <dsaini64@gmail.com>
2024-06-25 15:47:12 -0700

    adding task 3 modifications

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,54 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number, 
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  private static historicalData: { price_abc: number, price_def: number }[] = [];
+
+  private static getHistoricalAverageRatio(): number {
+    if (this.historicalData.length === 0) {
+      return 1; // default ratio if no historical data
+    }
+
+    const totalRatio = this.historicalData.reduce((acc, data) => acc + (data.price_abc / data.price_def), 0);
+    return totalRatio / this.historicalData.length;
   }
+
+  private static addHistoricalData(priceABC: number, priceDEF: number) {
+    this.historicalData.push({ price_abc: priceABC, price_def: priceDEF });
+    if (this.historicalData.length > 365) {
+      this.historicalData.shift();
+    }
+  }
+
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    this.addHistoricalData(priceABC, priceDEF);
+    const historicalAverageRatio = this.getHistoricalAverageRatio();
+    const upperBound = historicalAverageRatio * 1.10;
+    const lowerBound = historicalAverageRatio * 0.90;
+    return {
+      price_abc: priceABC, 
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp >serverRespond[1].timestamp ? serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+
+    };
+  
+  }
+
 }
